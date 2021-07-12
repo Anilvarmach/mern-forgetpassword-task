bbb255987ee1946b93c56e2b90d0465cfad2be1f
Anilvarmach <anilvarmach999@gmail.com>
2021-07-12 09:01:19 +0530

    forget password

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.DATABASE_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  });
+
+  console.log("MongoDB Connected");
+};
+
+module.exports = connectDB;
